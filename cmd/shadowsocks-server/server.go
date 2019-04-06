ac388c38bc0c21dbc9a8b941bc8393d4304fc3da
jinxmcg <cristian.malaia@gmail.com>
2019-04-06 19:52:18 +0200

    do commit

--- cmd/shadowsocks-server/server.go
+++ cmd/shadowsocks-server/server.go
@@ -428,17 +428,18 @@ func runUDP(port, password string) {
 }
 
 func enoughOptions(config *ss.Config) bool {
-	return config.ServerPort != 0 && config.Password != ""
+	return true //config.ServerPort != 0 && config.Password != ""
 }
 
 func unifyPortPassword(config *ss.Config) (err error) {
+
 	if len(config.PortPassword) == 0 { // this handles both nil PortPassword and empty one
 		if !enoughOptions(config) {
 			fmt.Fprintln(os.Stderr, "must specify both port and password")
 			return errors.New("not enough options")
 		}
-		port := strconv.Itoa(config.ServerPort)
-		config.PortPassword = map[string]string{port: config.Password}
+		//port := strconv.Itoa(config.ServerPort)
+		config.PortPassword = map[string]string{config.ServerPort: config.Password}
 	} else {
 		if config.Password != "" || config.ServerPort != 0 {
 			fmt.Fprintln(os.Stderr, "given port_password, ignore server_port and password option")
